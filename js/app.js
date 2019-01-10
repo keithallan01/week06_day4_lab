90c45e01a9b72cbe59bb301bbb3523465934355f
lacey <hiu@example.com>
2019-01-10 16:20:09 +0000

    add title, author, type

--- js/app.js
+++ js/app.js
@@ -1,3 +1,24 @@
 document.addEventListener('DOMContentLoaded', () => {
-  console.log('JavaScript loaded');
-})
+
+  const form = document.querySelector('#new-item-form');
+  form.addEventListener('submit', handleFormSubmit);
+  // form.reset();
+});
+
+const handleFormSubmit = function(event) {
+  event.preventDefault()
+  // console.log(event.target.title.value);
+  // console.log(event.target.author.value);
+  // console.log(event.target.category.value)
+  const readingList = document.querySelector('#reading-list');
+  const newItem = document.createElement('p')
+  newItem.textContent = `ADDED: ${event.target.title.value} AUTHOR: ${event.target.author.value}
+                        TYPE: ${event.target.category.value}`;
+  const form = document.querySelector('#new-item-form')
+    form.reset();
+  readingList.appendChild(newItem);
+
+};
+
+// const form = document.querySelector('#new-item-form')
+//   form.reset();
